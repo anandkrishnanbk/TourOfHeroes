b6b2f47c0c22e1710196f48bbfd3049093d8849e
anand.krishnankutty@bcbsnc.com <anand@anand-mbp.bcbsnc.com>
2017-08-15 11:03:27 -0400

    added files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,17 @@ export class Hero {
   id: number;
   name: string;
 }
-
+const HEROES: Hero[] = [
+  {id: 11, name: 'Mr. Nice'},
+  {id: 12, name: 'Mr. Nice'},
+  {id: 13, name: 'Narco'},
+  {id: 14, name: 'Bat man'},
+  {id: 15, name: 'Super man'},
+  {id: 16, name: 'Spider man'},
+  {id: 17, name: 'Captain America'},
+  {id: 18, name: 'Hulk'},
+  {id: 19, name: 'He man'},
+  {id: 20, name: 'Fire man'}];
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,9 +22,10 @@ export class Hero {
 
 export class AppComponent {
   title = 'Tour of Heroes';
+  heroes=HEROES;
   //hero = 'windstorm';
-  hero: Hero = {
+  /*hero: Hero = {
     id : 1,
     name:'Windstorm'
-  };
+  };*/
 }
