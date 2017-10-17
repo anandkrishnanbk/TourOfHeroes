974adeadcc14ffe63d9da4df9790c2791969f0e3
anand <anandkrishnanbk@gmail.com>
2017-10-17 14:24:36 -0400

    changed http files

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './Hero';
-import { HEROES } from './mock-heroes';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+//import { HEROES } from './mock-heroes';
 
 @Injectable()
 
 export class HeroService {
 
-  getHeroes(): Promise<Hero[]> {
-    return Promise.resolve(HEROES);
+  private heroesUrl='api/heroes';
+  constructor(private http: Http) { }
 
+  getHeroes(): Promise<Hero[]> {
+    return this.http.get(this.heroesUrl)
+    .toPromise()
+    .then(response => response.json().data as Hero[])
+    .catch(this.handleError);
+  //  return Promise.resolve(HEROES);
+  }
+
+  private handleError(error: any): Promise<any>
+  {
+    console.error('an error occured',error);
+    return Promise.reject(error.message || error);
   }
   getHeroesSlowly(): Promise<Hero[]> {
     return new Promise(resolve => {
